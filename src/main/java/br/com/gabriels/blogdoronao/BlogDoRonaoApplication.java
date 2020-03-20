0aae1bf501cbc29b67d0b22fe5cd2786e43c62a1
Gabriel Ronei <gabriheron@hotmail.com>
2020-03-19 21:35:49 -0300

    [Setup] Adicionando projeto e as telas iniciais

---
+++ src/main/java/br/com/gabriels/blogdoronao/BlogDoRonaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriels.blogdoronao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDoRonaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDoRonaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriels/blogdoronao/BlogDoRonaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriels.blogdoronao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDoRonaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
